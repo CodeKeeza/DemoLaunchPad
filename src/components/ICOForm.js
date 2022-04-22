a0026fb8f0be6f703b048d1a8e4b392f0aaeb8d4
keeza <CodeKeeza@protonmail.com>
2022-04-22 17:22:38 +0100

    form basics completed

--- src/components/ICOForm.js
+++ src/components/ICOForm.js
@@ -1,10 +1,153 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { ListItemText, Paper } from '@mui/material'
-import { Box } from '@mui/material'
-import { Form, TextArea } from 'web3uikit'
+import { Box, Slider } from '@mui/material'
+import { Form, TextArea, Button, Row, Typography, Card } from 'web3uikit'
 import CssBaseline from '@mui/material/CssBaseline'
 
 export default function ICOForm() {
+  const [liqPerc, setLiqPerc] = useState()
+  const [listingRate, setListingRate] = useState()
+
+  function handleLiqPerc(e) {
+    setLiqPerc(e.value)
+    console.log(e)
+    console.log(liqPerc)
+  }
+  function handleListingRate(e) {
+    setLiqPerc(e.value)
+    console.log(e)
+    console.log(liqPerc)
+  }
+  console.log(liqPerc)
+  function valuetext(value) {
+    return `${value}%`
+  }
+  const dial = (
+    <>
+      <div
+        style={{
+          marginBottom: '15px',
+          position: 'top',
+          display: 'flex-wrap',
+        }}
+      >
+        <div
+          style={{
+            border: 'none',
+            boxSizing: 'borderbox',
+            lineHeight: '1',
+            outline: 'none',
+            padding: '0',
+            borderRadius: '16px',
+            display: '-webkit-box',
+            display: '-webkit-flex',
+            display: '-ms-flexbox',
+            display: 'flex',
+            maxwidth: '100%',
+            minwidth: '-webkit-fit-content',
+            minwidth: '-moz-fit-content',
+            minwidth: 'fit-content',
+            outline: '1px solid',
+            position: 'relative',
+            justifyContent: 'space-between',
+            transition: 'all 0.1s linear',
+            outlineColor: '#C5CDD9',
+            height: '40px',
+            padding: '8px 16px',
+            marginBottom: '30px',
+            width: '800px',
+          }}
+        >
+          <Typography>
+            Percentage of raised tokens added to liquidity?
+          </Typography>
+          <div style={{ display: 'flex', marginTop: '5px' }}>{liqPerc}%</div>
+          <Box width={360}>
+            <Slider
+              defaultValue={25}
+              aria-label="Default"
+              valueLabelDisplay="auto"
+              getAriaValueText={valuetext}
+              step={1}
+              min={0}
+              max={100}
+              onChange={(e) => setLiqPerc(e.target.value)}
+            />
+          </Box>
+        </div>
+        <div style={{ marginLeft: '15px', marginTop: '10px' }}>
+          <Paper
+            style={{ width: 'auto', height: 'auto', backgroundColor: 'aqua' }}
+          >
+            // TODO: Calculations for caps and issuance
+          </Paper>
+        </div>
+      </div>
+      <div
+        style={{
+          marginBottom: '15px',
+          position: 'top',
+          display: 'flex-wrap',
+        }}
+      >
+        <div
+          style={{
+            border: 'none',
+            boxSizing: 'borderbox',
+            lineHeight: '1',
+            outline: 'none',
+            borderRadius: '16px',
+            display: '-webkit-box',
+            display: '-webkit-flex',
+            display: '-ms-flexbox',
+            display: 'flex',
+            maxwidth: '100%',
+            minwidth: '-webkit-fit-content',
+            minwidth: '-moz-fit-content',
+            minwidth: 'fit-content',
+            outline: '1px solid',
+            position: 'relative',
+            justifyContent: 'space-between',
+            transition: 'all 0.1s linear',
+            outlineColor: '#C5CDD9',
+            height: '40px',
+            marginBottom: '30px',
+            padding: '8px 16px',
+            width: '800px',
+          }}
+        >
+          <Typography>
+            Percentage of raised tokens added to liquidity?
+          </Typography>
+          <div style={{ display: 'flex', marginTop: '5px' }}>
+            {listingRate ? listingRate : 0}%
+          </div>
+          <Box style={{ width: '360px' }}>
+            <Slider
+              defaultValue={25}
+              aria-label="Default"
+              valueLabelDisplay="auto"
+              getAriaValueText={valuetext}
+              step={1}
+              min={0}
+              max={100}
+              onChange={(e) => setListingRate(e.target.value)}
+            />
+          </Box>
+        </div>
+        <div style={{ marginLeft: '15px', marginTop: '10px' }}>
+          <Paper
+            style={{ width: 'auto', height: 'auto', backgroundColor: 'aqua' }}
+          >
+            // TODO: Calculations for caps and issuance
+          </Paper>
+        </div>
+      </div>
+      <div style={{ display: 'flex-wrap' }}>
+        <Button type="submit" text="submit" theme="primary"></Button>
+      </div>
+    </>
+  )
   return (
     <>
       <CssBaseline />
@@ -16,75 +159,48 @@ export default function ICOForm() {
         data={[
           {
             inputWidth: '100%',
-            name: 'Token Name',
+            name: 'ILO name',
             type: 'text',
             value: '',
+            validation: {
+              required: true,
+            },
           },
           {
             inputWidth: '100%',
-            name: 'Token Address',
+            name: 'Token address',
             type: 'text',
             value: '',
-          },
-          {
-            name: 'your digits',
-            type: 'tel',
             validation: {
               required: true,
             },
-            value: '',
           },
           {
-            name: 'your password',
-            type: 'password',
-            validation: {
-              characterMaxLength: 20,
-              characterMinLength: 6,
-              required: true,
-            },
-            value: '',
-          },
-          {
-            name: 'Rate our form? 1-10',
+            name: 'How many tokens allocated to presale?',
             type: 'number',
-            validation: {
-              numberMax: 10,
-              numberMin: 1,
-              required: true,
-            },
             value: '',
-          },
-          {
-            name: 'pizza fav',
-            options: ['pineapple', 'peppers', 'chillies'],
-            type: 'box',
-            value: 'what toppings do you like?',
-          },
-          {
-            name: 'Morning checklist',
-            options: ['say GM', 'make coffee', 'build killer web3uiKit'],
-            type: 'switch',
             validation: {
               required: true,
             },
-            value: 'Check list',
           },
           {
-            name: 'pokemon',
-            options: ['charmander', 'squirtle', 'bulbasaur'],
-            type: 'radios',
-            value: "who's that pokemon?",
-          },
-          {
-            inputWidth: '100%',
-            name: 'Any more comments?',
-            type: 'textarea',
-            validation: {
-              required: true,
-            },
+            name: 'Soft Cap (Minimum per person)?',
+            type: 'number',
             value: '',
+            validation: {
+              required: true,
+            },
+          },
+          {
+            name: 'Hard Cap (Maximum per person)?',
+            type: 'number',
+            value: '',
+            validation: {
+              required: true,
+            },
           },
         ]}
+        customFooter={dial}
         onSubmit={function noRefCheck() {}}
         title="Test form"
       />
